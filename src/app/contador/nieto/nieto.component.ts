c2d8a00a7b78ed433b4270e02a5df9426b3f335a
Fran72 <francisco_llorente@hotmail.com>
2024-02-06 13:14:46 +0100

    padre-hijo-nieto...reset

--- src/app/contador/nieto/nieto.component.ts
+++ src/app/contador/nieto/nieto.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-nieto',
@@ -7,10 +7,27 @@ import { Component, OnInit } from '@angular/core';
   ]
 })
 export class NietoComponent implements OnInit {
-
+  @Input() contador: number;
+  @Output() cambioContador = new EventEmitter<number>();
+  
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  multiplicar() {
+    this.contador *= 2;
+    this.cambioContador.emit( this.contador );
+  }
+  
+  dividir() {
+    this.contador /= 2;
+    this.cambioContador.emit( this.contador );
+  }
+  
+  reset() {
+    this.contador = 0;
+    this.cambioContador.emit( this.contador );
+  }
+  
 }
